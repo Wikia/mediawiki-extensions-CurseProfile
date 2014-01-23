ca6d3cacf611b76eca91c15b3e83049214ee9826
Alex Smith <asmith@curse.com>
2014-01-23 10:16:44 -0600

    Removing unwanted deploys.
    --HG--
    branch : staging

--- upgrade/sql/add_profile_links.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE /*_*/user_profile
-ADD `up_steam_profile` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL AFTER `up_type`,
-ADD `up_xbl_profile` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL AFTER `up_type`,
-ADD `up_psn_profile` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL AFTER `up_type`;
