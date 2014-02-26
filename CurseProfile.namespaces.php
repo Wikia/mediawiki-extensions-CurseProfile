1be4c794b33a06a827e6670556dbe676c1aae67d
Alex Smith <asmith@curse.com>
2014-02-26 11:19:45 -0600

    Removing CurseProfile from this deploy.
    --HG--
    branch : iteration20140127

--- CurseProfile.namespaces.php
+++
@@ -1,7 +0,0 @@
-<?php
-$namespaceNames['en'] = array(
-	NS_USER_WIKI => 'UserWiki',
-	NS_USER_PROFILE => 'UserProfile',
-);
-$wgExtraNamespaces[NS_USER_WIKI]    = $namespaceNames['en'][NS_USER_WIKI];
-$wgExtraNamespaces[NS_USER_PROFILE] = $namespaceNames['en'][NS_USER_PROFILE];
--- upgrade/sql/add_favorite_wiki.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE /*_*/user_profile
-ADD `up_favorite_wiki` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL AFTER `up_type`;
