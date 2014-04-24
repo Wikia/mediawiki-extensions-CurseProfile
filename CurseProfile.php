647949e90efdbd0ed03944eae1ed1712a57003e9
Noah Manneschmidt <nmanneschmidt@Curse.com>
2014-04-23 19:19:35 -0500

    Move pagination style to a separate module
    --HG--
    branch : development

--- CurseProfile.php
+++ CurseProfile.php
@@ -82,7 +82,7 @@ $wgResourceModules['ext.curseprofile.profilepage'] = [
 	'scripts' => ['js/curseprofile.js'],
 	'localBasePath' => $extDir,
 	'remoteExtPath' => 'CurseProfile',
-	'dependencies' => ['ext.curseprofile.customskin', 'mediawiki.user', 'mediawiki.api', 'jquery.timeago'],
+	'dependencies' => ['ext.curseprofile.customskin', 'ext.curse.pagination', 'mediawiki.user', 'mediawiki.api', 'jquery.timeago'],
 ];
 $wgResourceModules['jquery.timeago'] = [
 	'scripts' => ['js/jquery.timeago.js'],
