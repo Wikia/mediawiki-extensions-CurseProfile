d022444a6ae61fd7df1485f479bd3a6ba5e8d627
Tim Aldridge <taldridge@curse.com>
2015-08-05 09:17:51 -0500

    LogFormatter base class WIP

---
+++ classes/CommentLogFormatter.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Curse Inc.
+ * Curse Profile
+ * A modular, multi-featured user profile system.
+ *
+ * @author		Tim Aldridge
+ * @copyright	(c) 2015 Curse Inc.
+ * @license		All Rights Reserved
+ * @package		CurseProfile
+ * @link		http://www.curse.com/
+ *
+ **/
+namespace CurseProfile;
+
+/**
+ * A class that will handle log formating for Recent Changes
+ */
+class CommentLogFormatter extends \LogFormatter {
+
+}
