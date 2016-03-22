7bd3c179d5481817fa7db09cd047dc348f63725c
Alexia E. Smith <washuu@gmail.com>
2016-03-22 13:36:10 -0500

    Context for moderators.

--- classes/ProfilePage.php
+++ classes/ProfilePage.php
@@ -341,7 +341,7 @@ class ProfilePage extends \Article {
 		global $wgOut, $wgUser;
 		$aboutText = $wgOut->parse($this->profile->getAboutText());
 		if (empty($aboutText)) {
-			$aboutText = \Html::element('em', [], wfMessage('empty_about_text')->plain());
+			$aboutText = \Html::element('em', [], wfMessage(($this->viewingSelf() ? 'empty_about_text' : 'empty_about_text_mod'))->plain());
 		}
 		if ($wgUser->isAllowed('profile-modcomments') || $this->viewingSelf()) {
 			$aboutText = \Html::rawElement(
