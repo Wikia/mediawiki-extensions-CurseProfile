34588cb66f1ee99b901d7b3fd0d68b8565ea44d4
Alexia E. Smith <washuu@gmail.com>
2016-03-23 17:42:56 -0500

    Syntax.

--- classes/ProfileData.php
+++ classes/ProfileData.php
@@ -305,7 +305,7 @@ class ProfileData {
 		$log->addEntry(
 			'profile-edited',
 			\Title::newFromURL('User:'.$target->getName()),
-			substr($comment, 0 140)."...",
+			substr($comment, 0, 140)."...",
 			['section' => $section],
 			$performer
 		);
