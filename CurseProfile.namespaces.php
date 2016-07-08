7d7bff8a1bddd836ced8de270f1ab0608c33ceb9
Cameron Chunn <cchunn@curse.com>
2016-07-08 14:59:05 -0500

    Temporary Redirect In Place Fam One Hundred Emoji

--- CurseProfile.namespaces.php
+++ CurseProfile.namespaces.php
@@ -1,5 +1,7 @@
 <?php
 $namespaceNames['en'] = array(
 	NS_USER_PROFILE => 'UserProfile',
+	NS_USER_WIKI => 'UserWiki',
 );
 $wgExtraNamespaces[NS_USER_PROFILE] = $namespaceNames['en'][NS_USER_PROFILE];
+$wgExtraNamespaces[NS_USER_WIKI] = $namespaceNames['en'][NS_USER_WIKI];
