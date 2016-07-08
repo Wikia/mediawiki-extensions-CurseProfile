e1a0e7db49c32edf0d5f33077f0671e47d4765ee
Alexia E. Smith <washuu@gmail.com>
2016-07-08 10:42:52 -0500

    Force skip cache on profile page.

--- classes/ProfilePage.php
+++ classes/ProfilePage.php
@@ -754,6 +754,7 @@ class ProfilePage extends \Article {
 
 		$classes = false;
 		if (!empty($this->user) && $this->user->getId()) {
+			$_cacheSetting = \Hydra\Subscription::skipCache(true);
 			$subscription = \Hydra\Subscription::newFromUser($this->user);
 			if ($subscription !== false) {
 				$classes = $subscription->getFlairClasses();
@@ -761,6 +762,7 @@ class ProfilePage extends \Article {
 					$classes = false; //Enforce sanity.
 				}
 			}
+			\Hydra\Subscription::skipCache($_cacheSetting);
 		}
 
 		return sprintf('
