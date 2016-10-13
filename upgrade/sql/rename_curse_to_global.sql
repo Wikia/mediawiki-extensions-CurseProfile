5f5f2d57b841200e170f28131b543fb2bf45d255
Alexia E. Smith <washuu@gmail.com>
2016-10-13 10:36:42 -0500

    That was a mess.

--- upgrade/sql/rename_curse_to_global.sql
+++ upgrade/sql/rename_curse_to_global.sql
@@ -1,11 +1,12 @@
 ALTER TABLE /*_*/user_board_reports CHANGE `ubr_reporter_curse_id` `ubr_reporter_global_id` INT(11) NOT NULL;
 ALTER TABLE /*_*/user_board_report_archives CHANGE `ra_curse_id_from` `ra_global_id_from` INT(11) NOT NULL DEFAULT '0';
 
-DROP INDEX /*i*/ubr_report_curse_id ON ON /*_*/user_board_reports;
-CREATE INDEX /*i*/ubr_report_global_id ON /*_*/user_board_report_archives (ubr_report_global_id);
+DROP INDEX /*i*/ubr_report_curse_id ON /*_*/user_board_reports;
+DROP INDEX /*i*/ubr_reporter_curse_id ON /*_*/user_board_reports;
+CREATE INDEX /*i*/ubr_reporter_global_id ON /*_*/user_board_reports (ubr_reporter_global_id);
 
-DROP INDEX /*i*/ubr_report_archive_id_ubr_reporter_curse_id ON ON /*_*/user_board_reports;
-ALTER TABLE /*_*/user_board_reports ADD UNIQUE KEY `ubr_report_archive_id_ubr_reporter_global_id` (`ubr_report_archive_id`,`ubr_reporter_global_id`)
+DROP INDEX /*i*/ubr_report_archive_id_ubr_reporter_curse_id ON /*_*/user_board_reports;
+ALTER TABLE /*_*/user_board_reports ADD UNIQUE KEY `ubr_report_archive_id_ubr_reporter_global_id` (`ubr_report_archive_id`,`ubr_reporter_global_id`);
 
-DROP INDEX /*i*/ra_curse_id_from ON ON /*_*/user_board_report_archives;
+DROP INDEX /*i*/ra_curse_id_from ON /*_*/user_board_report_archives;
 CREATE INDEX /*i*/ra_global_id_from ON /*_*/user_board_report_archives (ra_global_id_from);
