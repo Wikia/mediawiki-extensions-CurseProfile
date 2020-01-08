0ae82fd53e602e560a7ab525da78e86adc98d904
Alexia E. Smith <washuu@gmail.com>
2020-01-08 14:05:31 -0600

    Remove an old upgrade that breaks the migration.

--- classes/Hooks.php
+++ classes/Hooks.php
@@ -450,7 +450,6 @@ class Hooks {
 		$updater->addExtensionField('user_board', 'ub_in_reply_to', "{$extDir}/upgrade/sql/add_user_board_reply_to.sql", true);
 		$updater->addExtensionField('user_board', 'ub_edited', "{$extDir}/upgrade/sql/add_user_board_edit_and_reply_date.sql", true);
 		$updater->addExtensionField('user_board_report_archives', 'ra_action_taken_at', "{$extDir}/upgrade/sql/add_user_board_report_archives_action_taken_timestamp.sql", true);
-		$updater->addExtensionField('user_board', 'ub_admin_acted', "{$extDir}/upgrade/sql/add_user_board_admin_action_log.sql", true);
 
 		// global_id migration.
 		$updater->addExtensionUpdate(['addField', 'user_board_reports', 'ubr_reporter_user_id', "{$extDir}/upgrade/sql/user_board_reports/add_ubr_reporter_user_id.sql", true]);
--- upgrade/sql/add_user_board_admin_action_log.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE /*_*/user_board
-ADD `ub_admin_acted` int(11) DEFAULT NULL COMMENT 'Curse ID of admin who moderated',
-ADD `ub_admin_acted_at` datetime DEFAULT NULL;
