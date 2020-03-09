a1c4615a63478437af8f45fa72485bf904bb0f06
Alexia E. Smith <washuu@gmail.com>
2020-03-09 12:17:31 -0500

    Move Reverb into onRegistration.

--- classes/Hooks.php
+++ classes/Hooks.php
@@ -46,12 +46,38 @@ class Hooks {
 	 * @return boolean	True
 	 */
 	public static function onRegistration() {
-		global $wgExtraNamespaces;
+		global $wgExtraNamespaces, $wgReverbNotifications;
 
 		if (!defined('NS_USER_PROFILE')) {
 			define('NS_USER_PROFILE', 202);
 		}
 		$wgExtraNamespaces[NS_USER_PROFILE] = 'UserProfile';
+
+		$reverbNotifications = [
+			"user-interest-profile-comment" => [
+				"importance" => 8
+			],
+			"user-interest-profile-comment-reply-self-self" => [
+				"importance" => 8,
+				"use-preference" => "user-interest-profile-comment"
+			],
+			"user-interest-profile-comment-reply-self-other" => [
+				"importance" => 8,
+				"use-preference" => "user-interest-profile-comment"
+			],
+			"user-interest-profile-comment-reply-other-self" => [
+				"importance" => 8,
+				"use-preference" => "user-interest-profile-comment"
+			],
+			"user-moderation-profile-comment-report" => [
+				"importance" => 1,
+				"requires" => ["hydra_admin", "sysop"]
+			],
+			"user-interest-profile-friendship" => [
+				"importance" => 5
+			]
+		];
+		$wgReverbNotifications = array_merge($wgReverbNotifications, $reverbNotifications);
 
 		return true;
 	}
